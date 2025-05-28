e70e432ff41b9ab3ae54430973ce07cb8d62b713
rzrjck <razorree123@gmail.com>
2025-05-28 23:15:17 +0200

    keymap formatting, LTO to json

--- keyboards/keyball/keyball61/keymaps/graphite/keymap.c
+++ keyboards/keyball/keyball61/keymaps/graphite/keymap.c
@@ -79,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    SSNP_FRE , KC_F1    , KC_F2 ,    KC_F3    , KC_F4    , KC_F5    ,                                    KC_F6    ,   KC_F7,      KC_F8,      KC_F9 ,     KC_F10,     KC_F11   ,
+    SSNP_FRE , KC_F1    , KC_F2 ,    KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    ,   KC_F7,      KC_F8,      KC_F9 ,     KC_F10,     KC_F11   ,
     SSNP_VRT , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  KC_PGUP,     KC_HOME,    KC_UP,      KC_END,     KC_INS,     KC_F12,
     SSNP_HOR , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGDN,     KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_DEL,     XXXXXXX,
-    _______  , _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  ,   C(KC_LEFT), _______ ,   C(KC_RGHT), KC_BTN3  ,  _______  ,
+    _______  , _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  ,   C(KC_LEFT), _______ ,   C(KC_RGHT), _______  ,  _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             KC_DEL  , _______  ,   _______  ,  _______  ,  _______  ,  _______  ,  _______
   ),
 
